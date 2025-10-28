a1cff373213ac68cf22cf6152579ea9018f7dc7f
luongduy0906 <luongducduy0906@gmail.com>
2025-10-28 17:12:46 +0700

    fix signup link

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -4,6 +4,6 @@ class AppRoutes {
   static const String saved = '/saved';
   static const String history = '/history';
   static const String profile = '/profile';
-  static const String signup = 'signup';
+  static const String signup = '/signup';
   static const String login = '/login';
 }

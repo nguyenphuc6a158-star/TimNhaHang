46ac2eb647c9dbdf8d99d1c86d114c0f8f991002
ndpisme137 <nguyenphuc6a15.8@gmail.com>
2025-10-31 10:36:23 +0700

    tối ưu code bỏ form mẫu clean architecture

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -1,6 +1,5 @@
 class AppRoutes {
   static const String home = '/';
-  static const String note = '/note';
   static const String saved = '/saved';
   static const String history = '/history';
   static const String setting = '/setting';
